c5f2e41d334fffeac93ec2918ee7a5a08f386b6c
Derryblinks <db723252@gmail.com>
2025-08-04 03:19:57 +0000

    Added Triangle Type Identifier with 3-side logic

--- TriangleIdentifier/Program.cs
+++ TriangleIdentifier/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the lengths of the three sides of a triangle:");
+
+        Console.Write("Side 1: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Side 2: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Side 3: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        if (a == b && b == c)
+            Console.WriteLine("Triangle Type: Equilateral");
+        else if (a == b || b == c || a == c)
+            Console.WriteLine("Triangle Type: Isosceles");
+        else
+            Console.WriteLine("Triangle Type: Scalene");
+    }
+}
