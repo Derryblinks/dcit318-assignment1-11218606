3d9ec5499a593e1edeff5d5ea95a0fb082d028bf
Derryblinks <db723252@gmail.com>
2025-08-04 03:12:17 +0000

    Added Ticket Price Calculator with age-based discount logic

--- TicketPriceCalculator/Program.cs
+++ TicketPriceCalculator/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        int ticketPrice = (age <= 12 || age >= 65) ? 7 : 10;
+
+        Console.WriteLine($"Ticket Price: GHC{ticketPrice}");
+    }
+}
